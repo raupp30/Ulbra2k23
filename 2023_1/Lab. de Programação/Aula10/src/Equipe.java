7bb3bc54dc47e44d1286e360b8cbb8b7b586db9e
Joao <joaovraupp@rede.ulbra.br>
2023-05-08 19:56:22 -0300

    Atualizando TDE

--- 2023_1/Lab. de Programação/Aula10/src/Equipe.java
+++ 2023_1/Lab. de Programação/Aula10/src/Equipe.java
@@ -4,13 +4,17 @@ public class Equipe {
      int vitorias;
      int derrotas;
      int empates;
+     int pontos;
+     int kills;
 
      //Construtor
-    Equipe(String nome, int vitorias, int derrotas, int empates){
+    Equipe(String nome, int vitorias, int derrotas, int empates, int pontos){
         this.nome = nome;
         this.vitorias = 0;
         this.derrotas = 0;
         this.empates = 0;
+        this.pontos = 0;
+        this.kills = 0;
     }
     //Getters e setters
     public String getNome(){
@@ -38,14 +42,37 @@ public class Equipe {
         this.empates = empates;
     }
     //Incrementando atributo vitórias/derrotas/empates
-    public void registrarVitoria(){
+    public void registrarVitoria(int kills){
         this.vitorias++;
+        this.pontos += 3;
+        this.kills = this.kills + kills;
     }
-    public void registrarDerrota(){
+    public void registrarDerrota(int kills){
         this.derrotas++;
+        this.kills = this.kills + kills;
     }
-    public void registrarEmpate(){
+    public void registrarEmpate(int kills){
         this.empates++;
+        this.pontos ++;
+        this.kills = this.kills + kills;
+    }
+
+    public double calcAproveitamento(){
+        int totalDeJogos = this.vitorias + this.derrotas + this.empates;
+        int totalPontosPossiveis = totalDeJogos * 3;
+        double aproveitamento = 0;
+        if (totalPontosPossiveis > 0) {
+            aproveitamento = (double) pontos / totalPontosPossiveis * 100;
+        }return aproveitamento;
+    }
+    public void exibirInfo(){
+        System.out.println("Nome da equipe: "+this.nome);
+        System.out.println("Derrotas: "+this.derrotas);
+        System.out.println("Vitórias: "+this.vitorias);
+        System.out.println("Empates: "+this.empates);
+        System.out.println("Pontos: "+this.pontos);
+        System.out.println("Aproveitamento: "+this.calcAproveitamento());
+        System.out.println("Kills "+this.kills);
     }
 
 }
--- 2023_1/Lab. de Programação/Aula10/src/Main.java
+++ 2023_1/Lab. de Programação/Aula10/src/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args){
-        Equipe equipe1 = new Equipe("Equipe One", 0, 0, 0);
-        Equipe equipe2 = new Equipe("Equipe Two", 0, 0, 0);
+        Equipe equipe1 = new Equipe("Equipe One", 0, 0 ,0, 0);
+        Equipe equipe2 = new Equipe("Equipe Two", 0, 0, 0 , 0);
 
         //registrando vitorias/derrotas/empates
-        equipe1.registrarVitoria();
-        equipe1.registrarEmpate();
-        equipe2.registrarDerrota();
-        equipe2.registrarEmpate();
+        equipe1.registrarVitoria(130);
+        equipe1.registrarEmpate(95);
+        equipe2.registrarDerrota(56);
+        equipe2.registrarEmpate(88);
         //printando nome da equipe, vitorias/derrotas/empates
-        System.out.println("Equipe 1 - " +equipe1.getNome());
-        System.out.println("Vitórias - " +equipe1.vitorias);
-        System.out.println("Derrotas - " +equipe1.derrotas);
-        System.out.println("Empates - " +equipe1.empates);
 
-        System.out.println("Equipe 2 - " +equipe2.getNome());
-        System.out.println("Vitórias - " +equipe2.vitorias);
-        System.out.println("Derrotas - " +equipe2.derrotas);
-        System.out.println("Empates - " +equipe2.empates);
+
+        equipe1.exibirInfo();
+        System.out.println("-------------------------------------------");
+        equipe2.exibirInfo();
     }
 }
